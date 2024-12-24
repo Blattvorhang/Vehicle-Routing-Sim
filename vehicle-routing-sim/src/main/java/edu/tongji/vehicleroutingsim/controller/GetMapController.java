a462bca5200a6ebca179789797ce3a99bd35fee9
Kong Weitao <kevintungs@163.com>
2024-12-24 16:21:34 +0800

    新增获取地图map接口

---
+++ vehicle-routing-sim/src/main/java/edu/tongji/vehicleroutingsim/controller/GetMapController.java
@@ -0,0 +1,39 @@
+package edu.tongji.vehicleroutingsim.controller;
+
+import edu.tongji.vehicleroutingsim.service.MapService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description:
+ * <p>
+ * 获取地图map信息
+ * </p>
+ *
+ * @author KevinTung@Studyline
+ * @version 1.0
+ * @since 2024/12/24 16:18
+ */
+@RestController
+public class GetMapController {
+    private static final Logger logger = LoggerFactory.getLogger(InitPositionController.class);
+    private final MapService mapService;
+
+    @Autowired
+    public GetMapController(MapService mapService) {
+        this.mapService = mapService;
+    }
+
+    /**
+     * 获取地图信息
+     *
+     * @return 包含地图信息的 Map
+     */
+    @GetMapping("/getMap")
+    public boolean[][] getMap() {
+        return mapService.getMap();
+    }
+}
