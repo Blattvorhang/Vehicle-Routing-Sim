8d37e3bae322af11be5c6cb4cc93266a80ab9e1e
Kong Weitao <kevintungs@163.com>
2024-12-27 13:07:56 +0800

    编码设置

--- vehicle-routing-sim/src/main/java/edu/tongji/vehicleroutingsim/VehicleRoutingSimApplication.java
+++ vehicle-routing-sim/src/main/java/edu/tongji/vehicleroutingsim/VehicleRoutingSimApplication.java
@@ -1,10 +1,11 @@
 package edu.tongji.vehicleroutingsim;
 
-import edu.tongji.vehicleroutingsim.service.MapService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import edu.tongji.vehicleroutingsim.service.MapService;
 
 /**
  * Description:
