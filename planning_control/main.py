de3982f5ab4452b61a2a2b97af791ebe218c602c
Blattvorhang <blattvorhang@163.com>
2025-01-18 18:41:29 +0800

    Fixed the bug of repeatedly call init_backend()

--- planning_control/main.py
+++ planning_control/main.py
@@ -11,7 +11,6 @@ def main():
     # server_ip = "kevintung.cn"
     server_port = 8888
     system = System(engine, server_ip, server_port, visualize=True)
-    system.init_backend()
     
     print("Running system...")
     system.run()
